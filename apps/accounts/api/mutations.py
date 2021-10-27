d0ea35b100ad9319b135e9017cc686bfc2f87daa
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-27 10:01:10 -0400

    feat: mutation para obtener url de autenticacion

--- apps/accounts/api/mutations.py
+++ apps/accounts/api/mutations.py
@@ -1,6 +1,32 @@
 import graphene
+import tweepy
+from django.conf import settings
+from graphql import GraphQLError
 
 from apps.accounts.api.types import UserType
+
+
+class TwitterLoginUrl(graphene.Mutation):
+    """
+    Get Twitter login url
+    """
+    status = graphene.Boolean()
+    url = graphene.String()
+    oauth_token = graphene.String()
+
+    class Arguments:
+        callback_url = graphene.String(required=True)
+
+    @staticmethod
+    def mutate(root, info, callback_url):
+        auth = tweepy.OAuthHandler(settings.TWITTER_API_KEY, settings.TWITTER_API_KEY_SECRET, callback_url)
+
+        try:
+            redirect_url = auth.get_authorization_url()
+            return TwitterLoginUrl(status=True, url=redirect_url, oauth_token=auth.request_token['oauth_token'])
+        except tweepy.TweepyException as e:
+            print(e)
+            return GraphQLError('Error fetching Twitter authorization url')
 
 
 class TokenAuth(graphene.Mutation):
@@ -20,5 +46,8 @@ class TokenAuth(graphene.Mutation):
 
 
 class UserMutations(graphene.ObjectType):
+    # authenticate with twitter
+    twitter_login = TwitterLoginUrl.Field()
+
     # authenticate the User with its username or email and password to obtain the JSON Web token.
     token_auth = TokenAuth.Field()
--- config/settings/base.py
+++ config/settings/base.py
@@ -175,3 +175,7 @@ GRAPHENE = {
         'graphql_jwt.middleware.JSONWebTokenMiddleware',
     ],
 }
+
+# twitter secrets
+TWITTER_API_KEY = get_env_variable('TWITTER_API_KEY')
+TWITTER_API_KEY_SECRET = get_env_variable('TWITTER_API_KEY_SECRET')
