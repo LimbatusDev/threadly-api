3071b17d78a2eab10fd5511e27a1f1a6c47f4e8f
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-29 09:36:17 -0400

    minimal changes

--- apps/threads/models.py
+++ apps/threads/models.py
@@ -7,3 +7,4 @@ from apps.core.models import TimeStampedModel
 class Thread(TimeStampedModel):
     tweets = models.CharField('Tweets', max_length=2800)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # TODO: agregar fecha de publicación
