20cff00bdabaed8431fd4b2097a22b00e095f2b5
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-29 09:33:39 -0400

    feat: creando el modelo thread

--- apps/threads/models.py
+++ apps/threads/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.conf import settings
+
+from apps.core.models import TimeStampedModel
 
 
-class Thread(models.Model):
-    pass
+class Thread(TimeStampedModel):
+    tweets = models.CharField('Tweets', max_length=2800)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
