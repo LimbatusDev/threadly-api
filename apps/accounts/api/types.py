9666e00154027da99ea825a9dedff188eb32b899
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-30 09:26:42 -0400

    fix: devolviendo si el usuario es premium o no en el objeto User

--- apps/accounts/api/types.py
+++ apps/accounts/api/types.py
@@ -1,11 +1,18 @@
+import graphene
 from graphene_django import DjangoObjectType
 
 from apps.accounts import models
 
 
 class UserType(DjangoObjectType):
+    is_premium = graphene.Boolean()
+
     class Meta:
         model = models.User
+    
+    @staticmethod
+    def resolve_is_premium(parent, info):
+        return parent.is_premium()
 
 
 user_types = [
