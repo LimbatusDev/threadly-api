622f580cec83da031d1d5881af07d5f1bbd3e5a0
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-29 21:45:52 -0400

    feat: agregando app billing

---
+++ apps/billing/api/mutations.py
@@ -0,0 +1,2 @@
+class BillingMutations:
+    pass
--- config/settings/base.py
+++ config/settings/base.py
@@ -43,6 +43,7 @@ LOCAL_APPS = [
     'apps.api.apps.ApiConfig',
     'apps.core.apps.CoreConfig',
     'apps.threads.apps.ThreadsConfig',
+    'apps.billing.apps.BillingConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
