ab11e36055ce39b83390d124af2881cbb08ecf8a
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-01 14:46:42 -0400

    fix: variables de postgres

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,9 +14,9 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
         'CONN_MAX_AGE': 500
