3a87457cc54b2ca73bc7dfd12aff15640a169e3d
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-31 22:15:49 -0400

    fix: cambios minimos en el admin

--- apps/billing/admin.py
+++ apps/billing/admin.py
@@ -5,9 +5,11 @@ from . import models
 
 @admin.register(models.BillingConfig)
 class BillingConfigAdmin(admin.ModelAdmin):
-    list_field = ('price_monthly', 'price_yearly')
+    list_display = ('price_monthly', 'price_yearly')
 
 
 @admin.register(models.Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_field = ('user', 'number', 'amount', 'state')
+    list_display = ('number', 'user', 'amount', 'state')
+    list_filter = ['state']
+    search_fields = ['number']
