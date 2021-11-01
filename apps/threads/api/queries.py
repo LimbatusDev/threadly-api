17779d24824b563e66b473676627e544ac701682
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-01 15:37:50 -0400

    feat: query para obtener los hilos agendados

--- apps/threads/api/queries.py
+++ apps/threads/api/queries.py
@@ -1,5 +1,15 @@
+import graphene
+from django.db.models import Q
+from django.utils import timezone
+from graphql_jwt.decorators import login_required
+
 from .types import ThreadType
+from ..models import Thread
 
 
 class ThreadQueries:
-    pass
+    queues = graphene.List(ThreadType)
+
+    @login_required
+    def resolve_queues(self, info, **kwargs):
+        return Thread.objects.filter(Q(author=info.context.user) & Q(pub_date__gt=timezone.now()))
