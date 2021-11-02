a979a42862ae6b3212ff76723fc162c1e756c844
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-02 09:01:45 -0400

    feat: eliminar hilo programado futuro

--- apps/threads/api/mutations.py
+++ apps/threads/api/mutations.py
@@ -1,10 +1,15 @@
 import json
+import logging
 
 import graphene
+from django.db.models import Q
+from django.utils import timezone
 from graphql_jwt.decorators import login_required
 
 from apps.threads.models import Thread
 from apps.threads.utils import send_thread, schedule_thread
+
+logger = logging.getLogger(__name__)
 
 
 class TwitterThread(graphene.Mutation):
@@ -43,8 +48,30 @@ class ScheduleThread(graphene.Mutation):
         return ScheduleThread(status=True)
 
 
+class RemoveThread(graphene.Mutation):
+    status = graphene.Boolean()
+
+    class Arguments:
+        thread_id = graphene.Int()
+
+    @staticmethod
+    @login_required
+    def mutate(root, info, thread_id):
+        try:
+            thread = Thread.objects.get(Q(id=thread_id) & Q(pub_date__gte=timezone.now()))
+            # check if this thread belongs to author or is admin how remove it
+            if thread.author == info.context.user or info.context.user.is_superuser:
+                thread.delete()
+                return RemoveThread(status=True)
+        except Thread.DoesNotExist:
+            logger.warning('Future Thread with id {} no exist'.format(thread_id))
+        return RemoveThread(status=False)
+
+
 class ThreadMutations(graphene.ObjectType):
     # post twitter thread
     tweet_post = TwitterThread.Field()
     # schedule threads
     schedule_thread = ScheduleThread.Field()
+    # remove scheduled thread
+    remove_thread = RemoveThread.Field()
