9055c016beccb60be3f01c386c95764c0b9a1e14
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-05 11:16:47 -0400

    fix: update allowed host

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 # We load the env vars from a .env file
 load_dotenv()
 
-ALLOWED_HOSTS = ["api.threadly.app", "api.threadly.xyz"]
+ALLOWED_HOSTS = ["threadly-api.ragnarok22.dev"]
 DEBUG = False
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
